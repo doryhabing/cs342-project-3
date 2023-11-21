8ab7272c4f3a47c192d511546da36f5a44a1a1d1
doryhabing <doryhabing@gmail.com>
2023-11-21 09:56:58 -0600

    connected

--- src/main/java/GuiServer.java
+++ src/main/java/GuiServer.java
@@ -17,7 +17,7 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class GuiServer extends Application{
-	TextField s1,s2,s3,s4, c1;
+	TextField s1,s2,s3,s4, c1, port_prompt;
 	Button serverChoice,clientChoice,b1;
 	HashMap<String, Scene> sceneMap;
 	GridPane grid;
@@ -26,7 +26,6 @@ public class GuiServer extends Application{
 	Scene startScene;
 	BorderPane startPane;
 	Server serverConnection;
-	Client clientConnection;
 	ListView<String> listItems, listItems2;
 	
 	public static void main(String[] args) {
@@ -37,40 +36,43 @@ public class GuiServer extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("The Networked Client/Server GUI Example");
+		primaryStage.setTitle("ServerGUI");
+
+		this.port_prompt = new TextField();
+		this.port_prompt.setPromptText("Enter port number here and then press Start");
 		
-		this.serverChoice = new Button("Server");
+		this.serverChoice = new Button("Start");
 		this.serverChoice.setStyle("-fx-pref-width: 300px");
 		this.serverChoice.setStyle("-fx-pref-height: 300px");
 		
 		this.serverChoice.setOnAction(e->{ primaryStage.setScene(sceneMap.get("server"));
-											primaryStage.setTitle("This is the Server");
+											primaryStage.setTitle("Server Log");
+				String port = this.port_prompt.getText();
 				serverConnection = new Server(data -> {
 					Platform.runLater(()->{
 						listItems.getItems().add(data.toString());
 					});
-
-				});
+				}, port);
 											
 		});
 		
 		
-		this.clientChoice = new Button("Client");
-		this.clientChoice.setStyle("-fx-pref-width: 300px");
-		this.clientChoice.setStyle("-fx-pref-height: 300px");
+//		this.clientChoice = new Button("Client");
+//		this.clientChoice.setStyle("-fx-pref-width: 300px");
+//		this.clientChoice.setStyle("-fx-pref-height: 300px");
+//
+//		this.clientChoice.setOnAction(e-> {primaryStage.setScene(sceneMap.get("client"));
+//										   primaryStage.setTitle("This is a client");
+//										   clientConnection = new Client(data -> {
+//											   Platform.runLater(() -> {
+//												   listItems2.getItems().add(data.toString());
+//											   });
+//										   });
+//
+//										   clientConnection.start();
+//		});
 		
-		this.clientChoice.setOnAction(e-> {primaryStage.setScene(sceneMap.get("client"));
-										   primaryStage.setTitle("This is a client");
-										   clientConnection = new Client(data -> {
-											   Platform.runLater(() -> {
-												   listItems2.getItems().add(data.toString());
-											   });
-										   });
-							
-										   clientConnection.start();
-		});
-		
-		this.buttonBox = new HBox(400, serverChoice, clientChoice);
+		this.buttonBox = new HBox(400, port_prompt, serverChoice);
 		startPane = new BorderPane();
 		startPane.setPadding(new Insets(70));
 		startPane.setCenter(buttonBox);
@@ -82,12 +84,12 @@ public class GuiServer extends Application{
 		
 		c1 = new TextField();
 		b1 = new Button("Send");
-		b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
+		//b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
 		
 		sceneMap = new HashMap<String, Scene>();
 		
 		sceneMap.put("server",  createServerGui());
-		sceneMap.put("client",  createClientGui());
+		//sceneMap.put("client",  createClientGui());
 		
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
@@ -111,10 +113,10 @@ public class GuiServer extends Application{
 		return new Scene(pane, 500, 400);
 	}
 	
-	public Scene createClientGui() {
-		clientBox = new VBox(10, c1,b1,listItems2);
-		clientBox.setStyle("-fx-background-color: blue");
-
-		return new Scene(clientBox, 400, 300);
-	}
+//	public Scene createClientGui() {
+//		clientBox = new VBox(10, c1,b1,listItems2);
+//		clientBox.setStyle("-fx-background-color: blue");
+//
+//		return new Scene(clientBox, 400, 300);
+//	}
 }
