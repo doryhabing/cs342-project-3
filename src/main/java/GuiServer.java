187a3f33922968c78e146d71479250bf99f2a4b5
Maddy Pooley <mpool3@uic.edu>
2023-11-23 18:33:07 -0600

    update opening screen

--- src/main/java/GuiServer.java
+++ src/main/java/GuiServer.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -20,7 +21,8 @@ public class GuiServer extends Application{
 	TextField port_prompt;
 	Button serverChoice;
 	HashMap<String, Scene> sceneMap;
-	HBox buttonBox;
+	VBox buttonBox;
+	HBox serverBtn;
 	Scene startScene;
 	BorderPane startPane;
 	Server serverConnection;
@@ -40,8 +42,7 @@ public class GuiServer extends Application{
 		this.port_prompt.setPromptText("Enter port number here and then press Start");
 		
 		this.serverChoice = new Button("Start");
-		this.serverChoice.setStyle("-fx-pref-width: 300px");
-		this.serverChoice.setStyle("-fx-pref-height: 300px");
+		this.serverChoice.setPrefSize(250, 150);
 		
 		this.serverChoice.setOnAction(e->{ primaryStage.setScene(sceneMap.get("server"));
 											primaryStage.setTitle("Server Log");
@@ -54,12 +55,17 @@ public class GuiServer extends Application{
 											
 		});
 		
-		this.buttonBox = new HBox(400, port_prompt, serverChoice);
-		startPane = new BorderPane();
-		startPane.setPadding(new Insets(70));
+        this.serverBtn = new HBox(serverChoice);
+        serverBtn.setAlignment(Pos.CENTER);
+        this.buttonBox = new VBox(serverBtn, port_prompt);
+        startPane = new BorderPane();
+        
+		startPane.setPadding(new Insets(200));
 		startPane.setCenter(buttonBox);
-		
-		startScene = new Scene(startPane, 800,800);
+        
+        startPane.setStyle("-fx-background-color: lightBlue;");
+
+        startScene = new Scene(startPane, 900,600);
 		
 		listItems = new ListView<>();
 		listItems2 = new ListView<>();
